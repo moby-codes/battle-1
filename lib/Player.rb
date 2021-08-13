56dfd0ac2aea43a53df34d12c89c0efcc4b8d6b5
Hotu Nga Morehu Elkington <hotunz@gmail.com>
2021-08-13 16:43:36 +0100

    Added attack method and tests

--- lib/Player.rb
+++ lib/Player.rb
@@ -1,9 +1,19 @@
 class Player 
 
-attr_reader :name
+  attr_reader :name, :hit_points
+  DEFAULT_HP = 100
 
-  def initialize(name)
+  def initialize(name, hit_points = DEFAULT_HP)
     @name = name
+    @hit_points = hit_points
+  end
+
+  def attack(player)
+    player.receive_damage
+  end
+
+  def receive_damage
+    @hit_points -= 10
   end
 
 end
--- spec/Player_spec.rb
+++ spec/Player_spec.rb
@@ -1,14 +1,33 @@
 require 'Player'
 
-  describe Player do
+describe Player do
 
-    let(:player1) { described_class.new('Tom') }
+  let(:player1) { described_class.new('Tom') }
+  let(:player2) { described_class.new('Jerry') }
 
-    describe 'name' do
-      it 'returns name of player' do
-        expect(player1.name).to eq("Tom")
-      end
+  describe '#name' do
+
+    it 'returns name of player' do
+      expect(player1.name).to eq("Tom")
+    end
 
   end
+
+  describe '#receive_damage' do
+
+    it 'is expected to reduce hit_points by 10' do
+      expect{ player1.receive_damage }.to change{ player1.hit_points }.by (-10)
+    end
+
+  end
+
+  describe '#attack' do
+
+    it 'is expected to attack' do
+      expect{ player1.attack(player2) }.to change{ player2.hit_points }.by (-10)
+    end
+
+  end
+
 end
 
